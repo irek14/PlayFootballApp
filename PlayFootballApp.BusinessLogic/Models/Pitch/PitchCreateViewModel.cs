b05efeddd89413cd3ef3b306dec78580516f4b00
Ireneusz Stanicki <ireneusz.stanicki@itboom.pl>
2020-01-01 15:19:05 +0100

    Add simple create pitch view with leaflet map

---
+++ PlayFootballApp.BusinessLogic/Models/Pitch/PitchCreateViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PlayFootballApp.BusinessLogic.Models.Pitch
+{
+    public class PitchCreateViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Podaj nazwę boiska")]
+        [Display(Name = "Nazwa boiska")]
+        public string Name { get; set; }
+
+        [Display(Name = "Liczba miejsc")]
+        [Required(ErrorMessage = "Wpisz liczbę miejsc")]
+        [Range(1,int.MaxValue, ErrorMessage = "Liczba miejsc musi być większa od 0")]
+        public int SpotNumber { get; set; }
+
+        public decimal LocalisationX { get; set; }
+        public decimal LocalisationY { get; set; }
+    }
+}
---
+++ PlayFootballApp/Controllers/PitchController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PlayFootballApp.BusinessLogic.Models.Pitch;
+
+namespace PlayFootballApp.WWW.Controllers
+{
+    public class PitchController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new PitchCreateViewModel());
+        }
+    }
+}
