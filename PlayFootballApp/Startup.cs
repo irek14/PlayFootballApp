cb6f94812f4f726265ae3759490b174eb3e0caff
Ireneusz Stanicki <ireneusz.stanicki@itboom.pl>
2020-01-08 23:02:11 +0100

    Add main map to find events view

--- PlayFootballApp/Startup.cs
+++ PlayFootballApp/Startup.cs
@@ -72,7 +72,7 @@ namespace PlayFootballApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=FindEvents}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
